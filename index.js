86014843542b8ed6dc46d8eda19848d77a008e5a
Agustín Carignano <agustin.carignano@gmail.com>
2023-07-24 20:14:33 +0200

    put method

--- index.js
+++ index.js
@@ -3,37 +3,47 @@ import { express } from "./main.js";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+    res.send("Hello World!");
 });
 
 app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(id);
+    const id = req.params.id;
+    res.send(id);
 });
 
 app.get("/user", (req, res) => {
-  res.send("this is the user");
+    res.send("this is the user");
 });
 app.post("/user", (req, res) => {
-  const { email } = req.body;
-  res.json({ email });
+    const { email } = req.body;
+    res.json({ email });
 });
 app.post("/user/:id", (req, res) => {
-  const { id } = req.params;
-  const { email } = req.body;
-  res.json({ id, email });
+    const { id } = req.params;
+    const { email } = req.body;
+    res.json({ id, email });
 });
 
 app.get("/user/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(id);
+    const id = req.params.id;
+    res.send(id);
 });
 
 app.delete("/user/:id", (req, res) => {
-  const id = req.params.id;
-  res.send(id);
+    const id = req.params.id;
+    res.send(id);
+});
+
+app.put("/user", (req, res) => {
+    res.json({ greeting: "hi!" });
+});
+
+app.put("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({ greeting: "hi!", id, body });
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+    console.log("Listening on port 3000");
 });
